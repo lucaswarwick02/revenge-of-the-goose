4c17e39911649dfa3ab556600396df97de24c5ee
Ben Clothier <ben.clothier8@icloud.com>
2023-01-03 16:51:27 +0000

    Add map boundaries and camera view boundaries

--- Assets/Scripts/Minigame-1/CameraFollow.cs
+++ Assets/Scripts/Minigame-1/CameraFollow.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game.Utility;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
     public float zFollowOffset = -5.25f;
 
+    [SerializeField] private GameObject gooseBoundaryPrefab;
+
+    private void Awake()
+    {
+        UpdatePosition();
+
+        if (player)
+        {
+            float vp_y = Camera.main.WorldToViewportPoint(player.position).y;
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(1, vp_y, 1));
+            VectorMaths.LinePlaneIntersection(player.position, player.transform.forward, ray.origin, ray.direction, out Vector3 intersect);
+
+            Instantiate(gooseBoundaryPrefab, intersect, Quaternion.identity, transform);
+            Instantiate(gooseBoundaryPrefab, -intersect, Quaternion.identity, transform);
+        }
+    }
+
     void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
     {
         transform.position = new Vector3(0, transform.position.y, player.position.z + zFollowOffset);
     }
