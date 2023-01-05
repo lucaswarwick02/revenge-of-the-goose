b9ea8a4b39454658cf9db9f76042df3cf32e4db1
Lucas <lucaswarwick02@gmail.com>
2023-01-05 18:03:03 +0000

    Add comment randomizer based on number of animal kills

--- Assets/Scripts/Game/Animals/AnimalCommentController.cs
+++ Assets/Scripts/Game/Animals/AnimalCommentController.cs
@@ -6,6 +6,11 @@ public class AnimalCommentController : MonoBehaviour
 {
     private AnimalCommenter[] animalCommenters;
 
+    private Dictionary<int, List<string>> commentsDict = new Dictionary<int, List<string>>{
+        {1, new List<string>{"Thank you!", "Our Saviour!"}}
+    };
+    private List<string> currentComments;
+
     private void Awake()
     {
         GameHandler.OnSceneUpdated += GetListOfAnimalCommenters;
@@ -19,11 +24,17 @@ public class AnimalCommentController : MonoBehaviour
 
     private void OnAnimalKilled(int totalAnimalsKilled, Vector3 killEventPosition)
     {
+        List<string> attemptedComments;
+        if (commentsDict.TryGetValue(totalAnimalsKilled, out attemptedComments)) {
+            currentComments = attemptedComments;
+        }
+
         foreach (AnimalCommenter animalCommenter in animalCommenters)
         {
             if (animalCommenter.enabled)
             {
-                animalCommenter.Comment("You're killing us all!");
+                string randomComment = currentComments[Random.Range(0, currentComments.Count)];
+                animalCommenter.Comment(randomComment);
             }
         }
     }
