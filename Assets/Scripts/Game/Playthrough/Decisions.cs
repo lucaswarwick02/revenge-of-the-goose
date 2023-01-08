2ad83157801b8a47bc4362e62f9cff40e96e0043
Ben Clothier <ben.clothier8@icloud.com>
2023-01-08 17:11:05 +0000

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Game/Playthrough/Decisions.cs
+++ Assets/Scripts/Game/Playthrough/Decisions.cs
@@ -10,11 +10,19 @@ public static class Decisions
 
     private static Dictionary<string, List<string>> conversationResponses = new ();
 
+    public static bool ConversationWasStarted(string conversationID) => conversationResponses.ContainsKey(conversationID);
+
     public static bool PlayerResponded(string conversationID, string responseID) => conversationResponses.TryGetValue(conversationID, out List<string> responses) && responses.Contains(responseID);
+
+    public static void RecordConversationStarted(string conversationID)
+    {
+        // Add new empty entry
+        conversationResponses.Add(conversationID, new List<string>());
+    }
 
     public static void RecordPlayerResponse(string conversationID, string responseID)
     {
-        // Check if conversation already has an entry
+        // Check if conversation already has an entry (meaning it has started)
         if (conversationResponses.TryGetValue(conversationID, out List<string> responses))
         {
             // Get list of responses and add new response
@@ -23,8 +31,7 @@ public static class Decisions
         }
         else
         {
-            // Add new entry
-            conversationResponses.Add(conversationID, new List<string>() { responseID });
+            Debug.LogError("Conversation has not been recorded to have been started.");
         }
     }
 }
