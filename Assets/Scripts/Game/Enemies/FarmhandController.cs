9647d1cc57baeea5105f7dce06dadd4989a673ef
Ben Clothier <ben.clothier8@icloud.com>
2023-01-08 21:24:54 +0000

    Implement farmhand shooting

---
+++ Assets/Scripts/Game/Enemies/FarmhandController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FarmhandController : MonoBehaviour
+{
+    [SerializeField] private float attackRange = 15;
+    [SerializeField] private float attackInterval = 3;
+    [SerializeField] private float attackDamage = 20;
+
+
+    private Rigidbody rb;
+    private Animator anim;
+    private AudioSource audio;
+
+    private Transform player;
+    private float nextAttackTime;
+
+    public bool IsDead { get; set; }
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    private void Update()
+    {
+        if (!IsDead)
+        {
+            if (InAttackRangeOfPlayer() && Time.time >= nextAttackTime)
+            {
+                nextAttackTime = Time.time + attackInterval;
+                AttackPlayer();
+            }
+        }
+    }
+
+    private bool InAttackRangeOfPlayer()
+    {
+        return (player.transform.position - transform.position).magnitude <= attackRange;
+    }
+
+    private void AttackPlayer()
+    {
+        audio.Play();
+        anim.SetTrigger("Attack");
+        PlayerHealth.InflictDamage(attackDamage, transform.position);
+    }
+
+    public void StartDeath(RaycastHit hitInfo, Vector3 origin)
+    {
+        IsDead = true;
+        anim.SetTrigger("Death");
+
+        Vector3 impactDir = Vector3.RotateTowards((hitInfo.point - origin).normalized, Vector3.up, Mathf.Deg2Rad * 30, 0);
+        rb.velocity = impactDir * 4;
+
+        enabled = false;
+    }
+
+    private void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
