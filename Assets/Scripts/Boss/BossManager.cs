7f54c6505edb0a0bd05f28ab5a93b9f3f3c203a5
Lucas <lucaswarwick02@gmail.com>
2023-01-09 16:58:12 +0000

    Farmhand monologue after boss battle

--- Assets/Scripts/Boss/BossManager.cs
+++ Assets/Scripts/Boss/BossManager.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class BossManager : MonoBehaviour
 {
-    //
+    [SerializeField] private GameObject farmhandBossPrefab;
+
+    private void OnEnable() {
+        GetComponent<Destructible>().OnDestroyed.AddListener(CreateFarmhandBoss);
+    }
+
+    private void OnDisable() {
+        GetComponent<Destructible>().OnDestroyed.RemoveListener(CreateFarmhandBoss);
+    }
+
+    private void CreateFarmhandBoss (RaycastHit hit, Vector3 vec) {
+        // Remove all Farmhands
+        foreach(FarmhandController farmhand in GameObject.FindObjectsOfType<FarmhandController>()) {
+            Destroy(farmhand.gameObject);
+        }
+        // Remove all Dogs
+        foreach(DogController dog in GameObject.FindObjectsOfType<DogController>()) {
+            Destroy(dog.gameObject);
+        }
+
+        Instantiate(farmhandBossPrefab, transform.position, Quaternion.identity);
+    }
 }
