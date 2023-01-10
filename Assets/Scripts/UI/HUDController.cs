e858252ed49d0c794efb16cf408b821eed13a97a
Ben Clothier <ben.clothier8@icloud.com>
2023-01-10 13:17:14 +0000

    Disable HUD on conversation

--- Assets/Scripts/UI/HUDController.cs
+++ Assets/Scripts/UI/HUDController.cs
@@ -8,12 +8,16 @@ public class HUDController : MonoBehaviour
     {
         GameHandler.OnGameOver += Deactivate;
         GameHandler.OnGameLoaded += Activate;
+        Converser.OnStartConversation += Deactivate;
+        Converser.OnEndConversation += Activate;
     }
 
     private void OnDisable()
     {
         GameHandler.OnGameOver -= Deactivate;
         GameHandler.OnGameLoaded -= Activate;
+        Converser.OnStartConversation -= Deactivate;
+        Converser.OnEndConversation -= Activate;
     }
 
     private void Deactivate(Transform killer)
